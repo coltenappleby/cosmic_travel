30e81506cb7b6fb0202feb0240fe21abd82f5036
coltenappleby <colten.appleby@gmail.com>
2021-03-23 23:28:56 -0400

    create scientist, edit and update scientist. Updated show page to conform better this instructions

--- app/controllers/scientists_controller.rb
+++ app/controllers/scientists_controller.rb
@@ -8,4 +8,43 @@ class ScientistsController < ApplicationController
         @scientist = Scientist.find(params[:id])
     end
 
+    def new
+        @scientist = Scientist.new
+    end
+
+    def create
+        @scientist = Scientist.create(scientist_params)
+
+        if @scientist.valid?
+            redirect_to scientist_path(@scientist)
+        else
+            redirect_to new_scientist_path
+        end
+
+
+    end
+
+    def edit
+        @scientist = Scientist.find(params[:id])
+    end
+
+    def update
+        @scientist = Scientist.find(params[:id])
+        @scientist.update(scientist_params)
+
+        if @scientist.valid?
+            redirect_to scientist_path(@scientist)
+        else
+            redirect_to edit_scientist_path
+        end
+
+    end
+
+
+    private
+
+    def scientist_params
+        params.require(:scientist).permit(:name, :field_of_study)
+    end
+
 end
--- app/models/scientist.rb
+++ app/models/scientist.rb
@@ -3,4 +3,7 @@ class Scientist < ApplicationRecord
     has_many :missions
     has_many :planets, through: :missions
 
+    validates :name, uniqueness: true
+    validates :name, :field_of_study, presence: true
+
 end
